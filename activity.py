da2bb496405fb5abbb9578d62b5044243ee3768a
bishoy <bishoyw.fathy@gmail.com>
2025-06-23 12:44:41 +0300

    add help button

--- activity.py
+++ activity.py
@@ -1,7 +1,8 @@
 from sugar3.activity import activity
 from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.activity.widgets import ActivityToolbarButton, StopButton
-from gi.repository import Gtk
+from sugar3.graphics.toolbutton import ToolButton
+from gi.repository import Gtk, Gdk, Pango
 from glview import GLView 
 
 class SomaCube(activity.Activity):
@@ -20,6 +21,17 @@ class SomaCube(activity.Activity):
         # Separator
         separator = Gtk.SeparatorToolItem()
         separator.props.draw = False
+        toolbar_box.toolbar.insert(separator, -1)
+        
+        # Help button
+        help_button = ToolButton('toolbar-help')
+        help_button.set_tooltip('Help')
+        help_button.connect('clicked', self._show_help)
+        toolbar_box.toolbar.insert(help_button, -1)
+        
+        # Separator
+        separator = Gtk.SeparatorToolItem()
+        separator.props.draw = False
         separator.set_expand(True)
         toolbar_box.toolbar.insert(separator, -1)
         
@@ -32,21 +44,67 @@ class SomaCube(activity.Activity):
     
     def _setup_content(self):
         # Create main container
-        main_box = Gtk.VBox()
+        self.main_box = Gtk.VBox()
         
         # Create our OpenGL view
         self.gl_view = GLView()
         self.gl_view.set_size_request(600, 400)
         
         # Add some instructions
-        instructions = Gtk.Label()
-        instructions.set_markup(
+        self.instructions = Gtk.Label()
+        self.instructions.set_markup(
             "<span size='large'>Rotate view with mouse</span>")
-        instructions.set_halign(Gtk.Align.CENTER)
-        instructions.set_margin_bottom(10)
+        self.instructions.set_halign(Gtk.Align.CENTER)
+        self.instructions.set_margin_bottom(10)
         
-        main_box.pack_start(self.gl_view, True, True, 0)
-        main_box.pack_start(instructions, False, False, 0)
+        self.main_box.pack_start(self.gl_view, True, True, 0)
+        self.main_box.pack_start(self.instructions, False, False, 0)
         
-        self.set_canvas(main_box)
-        main_box.show_all()
+        self.set_canvas(self.main_box)
+        self.main_box.show_all()
+    
+    def _show_help(self, button):
+        """Show help dialog"""
+        dialog = Gtk.Dialog(
+            title="Soma Cube Help",
+            parent=self,
+            flags=Gtk.DialogFlags.MODAL,
+            buttons=("_Close", Gtk.ResponseType.CLOSE)
+        )
+        
+        # Set dialog size
+        dialog.set_default_size(400, 300)
+        
+        # Create text view for help content
+        textview = Gtk.TextView()
+        textview.set_editable(False)
+        textview.set_cursor_visible(False)
+        textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        
+        # Set font
+        font_desc = Pango.FontDescription("Sans 12")
+        textview.modify_font(font_desc)
+        
+        # Add help text
+        help_text = """Soma Cube Help:
+
+1. Left-click and drag to rotate the cube
+2. Right-click and drag to pan the view
+3. Scroll to zoom in/out
+4. Press R to reset the view"""
+        
+        buffer = textview.get_buffer()
+        buffer.set_text(help_text)
+        
+        # Add to dialog
+        scrolled = Gtk.ScrolledWindow()
+        scrolled.set_policy(
+            Gtk.PolicyType.AUTOMATIC, 
+            Gtk.PolicyType.AUTOMATIC)
+        scrolled.add(textview)
+        dialog.get_content_area().pack_start(scrolled, True, True, 0)
+        
+        # Show dialog
+        dialog.show_all()
+        dialog.run()
+        dialog.destroy()
